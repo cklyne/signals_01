c718fe53deddc81d55a76a60faa432b3c7f91743
Kostya <65129914+cklyne@users.noreply.github.com>
2024-09-26 14:37:57 +0200

    Stonks model
    Implemented a model class for handling stock-like models.

--- signals_class/sg_class/signal_classes.py
+++ signals_class/sg_class/signal_classes.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Optional
 
 import jax
@@ -76,6 +77,68 @@ class SignalBase(jft.Model):
         res = jnp.exp(self.cf(x))
         res = res[self.pad_left : -self.pad_right]
         return res
+
+
+class StonksModel(jft.Model):
+    def __init__(
+        self,
+        nt: int,
+        dt: float = 1.0,
+        name: str = "",
+        avg_yearly_yield: float = 1.05,
+        std_yearly_yield: float = 0.05,
+        mean_fluctuation_amplitude: float = 0.20,
+        std_fluctuation_amplitude: float = 0.05,
+        ohp_gamma: float = 0.1,
+        starting_price_mean: float = 3000.0,
+        starting_price_std: float = 1.0,
+    ):
+        self.nt = nt
+        self.t = jnp.arange(self.nt)
+        self.dt = dt
+        self.name = name
+
+        self.yearly_yield = jft.LogNormalPrior(
+            mean=avg_yearly_yield,
+            std=std_yearly_yield,
+            name=f"{self.name}_yearly_yield",
+        )
+        self.gp_fluctuations = jft.OrnsteinUhlenbeckProcess(
+            sigma=jft.LogNormalPrior(
+                mean=mean_fluctuation_amplitude / 3,
+                std=std_fluctuation_amplitude / 3,
+                name=f"{self.name}_fluctuation_amplitude",
+            ),
+            gamma=ohp_gamma,
+            dt=self.dt,
+            N_steps=self.nt,
+            x0=0.0,
+        )
+        self.starting_price = jft.LogNormalPrior(
+            mean=starting_price_mean,
+            std=starting_price_std,
+            name=f"{self.name}_starting_price",
+        )
+        super().__init__(
+            init=partial(
+                jft.random_like,
+                primals=self.yearly_yield.domain.copy()
+                | self.gp_fluctuations.domain
+                | self.starting_price.domain,
+            )
+        )
+
+    def __call__(self, x):
+        starting_price_val = self.starting_price(x)
+
+        yearly_yield = self.yearly_yield(x)
+        daily_yield = jnp.float_power(yearly_yield, 1 / 365)
+        log_price_slope = jnp.log(daily_yield)
+        log_price_pred = log_price_slope * self.t
+
+        price_fluctuations = self.gp_fluctuations(x)
+
+        return starting_price_val * jnp.exp(log_price_pred) * (1.0 + price_fluctuations)
 
 
 # class SignalRes(jft.Model):
